c5e7dfa122635d052a783a2009841e0983d9052d
Angel Paunchev <apaunchev@gmail.com>
2015-02-25 16:59:33 +0000

    Add GET and POST routes for /users

--- app/routes/api.js
+++ app/routes/api.js
@@ -11,5 +11,37 @@ module.exports = function(app, express) {
         res.json({ message: 'Hello world.' });
     });
 
+    // API: /users
+    apiRouter.route('/users')
+
+        // get all users
+        .get(function(req, res) {
+            User.find(function(err, users) {
+                if (err) res.send(err);
+
+                res.json(users);
+            });
+        })
+
+        // create a user
+        .post(function(req, res) {
+            console.log(req);
+            var user = new User();
+            user.username = req.body.username;
+            user.password = req.body.password;
+
+            user.save(function(err) {
+                if (err) {
+                    // duplicate entry
+                    if (err.code == 11000)
+                        return res.json({ success: false, message: 'A user with that username already exists.' });
+                    else
+                        return res.send(err);
+                }
+
+                res.json({ success: true, message: 'User created.' });
+            });
+        });
+
     return apiRouter;
 };
