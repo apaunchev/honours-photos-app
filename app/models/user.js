2d0ad5937e29422372f51c322242160dbf091135
Angel Paunchev <apaunchev@gmail.com>
2015-03-14 17:49:30 +0000

    Add comparePassword method

--- app/models/user.js
+++ app/models/user.js
@@ -29,4 +29,11 @@ UserSchema.pre('save', function(next) {
     });
 });
 
+// method to compare a given password with the database hash
+UserSchema.methods.comparePassword = function(password) {
+    var user = this;
+
+    return bcrypt.compareSync(password, user.password);
+};
+
 module.exports = mongoose.model('User', UserSchema);
