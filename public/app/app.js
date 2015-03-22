19956c97ec975ae66b3d164069d9d6b66fd76e43
Angel Paunchev <apaunchev@gmail.com>
2015-03-22 12:46:10 +0000

    Add confirmation dialogs when deleting photos

--- public/app/app.js
+++ public/app/app.js
@@ -7,4 +7,20 @@ angular.module('app', [
     .config(function($httpProvider) {
         // attach the auth interceptor to all http requests
         $httpProvider.interceptors.push('AuthInterceptor');
-    });
+    })
+
+    .directive('ngConfirmClick', [
+        function() {
+            return {
+                link: function(scope, element, attr) {
+                    var msg = attr.ngConfirmClick || "Are you sure?";
+                    var clickAction = attr.confirmedClick;
+
+                    element.bind('click', function(event) {
+                        if (window.confirm(msg))
+                            scope.$eval(clickAction);
+                    });
+                }
+            };
+        }
+    ]);
